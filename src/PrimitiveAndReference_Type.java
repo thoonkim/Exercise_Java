f1ab054b48bed24ad4474f20ae4c0cd579ecebb5
thoonkim <thoon.kim12@gmail.com>
2022-02-14 12:49:23 +0900

    220214 Commit

---
+++ src/PrimitiveAndReference_Type.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class PrimitiveAndReference_Type {
+    /*기본형(Primitive type*/
+    /*8개의 기본형이 존재한다
+    * boolean(논리형), char(문자형), byte(1byte), short(2byte), int(4byte), long(8byte)*/
+
+    /*참조형(Reference type*/
+    /*기본형을 제외한 나머지*/
+
+}
